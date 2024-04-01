e65eb5715aa358e1b429e07140b6b187bafdd57b
Nyi Nyi Zin <nyinyizin1818@gmail.com>
2024-04-01 12:07:34 +0630

    use Reducer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useReducer } from "react";
+
+const firstReducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.PLUS:
+      return { ...state, count: state.count + 1 };
+    case ACTIONS.MINUS:
+      return { ...state, count: state.count - 1 };
+    case ACTIONS.UPDATE_KEY:
+      return { ...state, key: action.payload };
+    default:
+      throw new Error();
+  }
+};
+
+const ACTIONS = {
+  PLUS: "plus",
+  MINUS: "minus",
+  UPDATE_KEY: "updateKey",
+};
 
 export default function Home() {
-  const [key, setKey] = useState("");
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(firstReducer, { key: "", count: 0 });
 
   return (
     <div>
       <input
         type="text"
-        onChange={(event) => {
-          setKey(event.currentTarget.value);
-        }}
+        onChange={(e) =>
+          dispatch({ type: "updateKey", payload: e.target.value })
+        }
       />
-      <p onChange={() => {}}>Your Key is - {key}</p>
-      <br />
-      <br />
-      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
-      <span>{count}</span>
-      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <p>Your Key is - {state.key} </p>
+      <br></br>
+      <button onClick={() => dispatch({ type: "plus" })}>+</button>
+      <span>{state.count}</span>
+      <button onClick={() => dispatch({ type: "minus" })}>-</button>
     </div>
   );
 }
